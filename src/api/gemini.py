7b81d1eac0dcd602bc25fbe8bfe5dfbf58f9e6e9
AvFl <82409897+AvFl4028@users.noreply.github.com>
2025-01-06 19:24:17 -0600

    json format and promps

--- src/api/gemini.py
+++ src/api/gemini.py
@@ -1,4 +1,14 @@
 import google.generativeai as genai
+from api.promps import promp
+from enum import Enum
+import json
+
+
+class HistoryType(Enum):
+    REDDIT = 0
+    HORROR = 1
+    SPACE = 2
+
 
 class GeminiAPI:
     def __init__(self, key, model="gemini-1.5-flash"):
@@ -6,8 +16,17 @@ class GeminiAPI:
         self.model = model
         genai.configure(api_key=self.key)
 
-
-    def response(self, promp: str = "Explain how AI works"):
+    def response(self, history_type: HistoryType = 0, json_type: bool = False):
         response = genai.GenerativeModel(self.model)
-        response = response.generate_content(promp)
-        return response.text
+        match history_type:
+            case HistoryType.REDDIT:
+              response = response.generate_content(promp)
+            case HistoryType.HORROR:
+              return "Horror History In Development"
+            case HistoryType.SPACE:
+              return "Space History In Development"
+        response = response.text.replace("```json", "").replace("```", "")
+        if json_type:
+          return json.loads(response)
+        else:
+          return response
---
+++ src/api/promps.py
@@ -0,0 +1,17 @@
+promp = """
+Make only one reddit history of 2 minutes long, then divide the history in two parts, after that, in only one json file format, without anything else.
+In the history exclude all the images and videos, only the text is needed.
+The json format should be like this:
+{
+  "user": "{generate a random username}",
+  "reddit_history": {
+    "title": "{Title of the reddit history}",
+    "part1": {
+      "content": "{Content of the post and need to be 1 minute long}"
+    },
+    "part2": {
+      "content": "{Content of the post and need to be 1 minute long}"
+    }
+  }
+}
+"""
